04044f583d101b743a10852af26a908ca769965c
rodrimati1992 <rodrimatt1985@gmail.com>
2021-03-22 06:15:26 -0300

    Made the byte slice functions not require features.
    Moved byte slice comparison fns to slice module,
    renamed them to `eq_bytes`, `cmp_bytes`, `eq_option_bytes`, `cmp_option_bytes`.
    Added doc examples, and reexported back in the `slice::cmp` module.
    
    Added comparison functions for `&[usize]`, `&[isize]`, `Option<&[usize]>`, `Option<&[isize]>`

--- konst/src/slice.rs
+++ konst/src/slice.rs
@@ -8,3 +8,71 @@ pub mod cmp;
 mod slice_const_methods;
 
 pub use slice_const_methods::*;
+
+__declare_slice_cmp_fns! {
+    import_path = "konst",
+
+    (
+        ///
+        ///  # Example
+        ///
+        ,
+        /// ```rust
+        /// use slice::slice::eq_bytes;
+        ///
+        /// const FOO: &[u8] = b"foo";
+        /// const BAR: &[u8] = b"fooooo";
+        /// const BAZ: &[u8] = b"bar";
+        ///
+        ///
+        /// const FOO_EQ_FOO: bool = eq_str(FOO, FOO);
+        /// assert!( FOO_EQ_FOO );
+        ///
+        /// const FOO_EQ_BAR: bool = eq_str(FOO, BAR);
+        /// assert!( !FOO_EQ_BAR );
+        ///
+        /// const FOO_EQ_BAZ: bool = eq_str(FOO, BAZ);
+        /// assert!( !FOO_EQ_BAZ );
+        ///
+        /// ```
+        ///
+        ,
+        /// ```rust
+        /// use slice::slice::cmp_bytes;
+        ///
+        /// const FOO: &[u8] = b"foo";
+        /// const BAR: &[u8] = b"fooooo";
+        /// const BAZ: &[u8] = b"bar";
+        ///
+        ///
+        /// const FOO_CMP_FOO: Ordering = cmp_str(FOO, FOO);
+        /// assert_eq!(FOO_CMP_FOO, Ordering::Equal);
+        ///
+        /// const FOO_CMP_BAR: Ordering = cmp_str(FOO, BAR);
+        /// assert_eq!(FOO_CMP_BAR, Ordering::Less);
+        ///
+        /// const FOO_CMP_BAZ: Ordering = cmp_str(FOO, BAZ);
+        /// assert_eq!(FOO_CMP_BAZ, Ordering::Greater);
+        ///
+        /// ```
+        ///
+        ,
+        u8,
+        eq_bytes,
+        cmp_bytes,
+    )
+}
+
+__declare_fns_with_docs! {
+    (Option<&'a [u8]>, (eq_option_bytes, cmp_option_bytes))
+
+    docs(default)
+
+    macro = __impl_option_cmp_fns!(
+        for['a,]
+        params(l, r)
+        eq_comparison = eq_bytes(l, r),
+        cmp_comparison = cmp_bytes(l, r),
+        parameter_copyability = copy,
+    ),
+}
--- konst/src/slice/cmp.rs
+++ konst/src/slice/cmp.rs
@@ -1,19 +1,26 @@
+#[doc(inline)]
+pub use crate::slice::{
+    cmp_bytes as cmp_slice_u8, cmp_option_bytes as cmp_option_slice_u8, eq_bytes as eq_slice_u8,
+    eq_option_bytes as eq_option_slice_u8,
+};
+
 use core::cmp::Ordering;
 
 __declare_slice_cmp_fns! {
     import_path = "konst",
 
-    (,,, u8, eq_slice_u8, cmp_slice_u8,)
     (,,, u16, eq_slice_u16, cmp_slice_u16,)
     (,,, u32, eq_slice_u32, cmp_slice_u32,)
     (,,, u64, eq_slice_u64, cmp_slice_u64,)
     (,,, u128, eq_slice_u128, cmp_slice_u128,)
+    (,,, usize, eq_slice_usize, cmp_slice_usize,)
 
     (,,, i8, eq_slice_i8, cmp_slice_i8,)
     (,,, i16, eq_slice_i16, cmp_slice_i16,)
     (,,, i32, eq_slice_i32, cmp_slice_i32,)
     (,,, i64, eq_slice_i64, cmp_slice_i64,)
     (,,, i128, eq_slice_i128, cmp_slice_i128,)
+    (,,, isize, eq_slice_isize, cmp_slice_isize,)
 
     (,,, bool, eq_slice_bool, cmp_slice_bool,)
     (,,, char, eq_slice_char, cmp_slice_char,)
@@ -56,16 +63,17 @@ __delegate_const_ord! {
 }
 
 __declare_fns_with_docs! {
-    (Option<&'a [u8]>, (eq_option_slice_u8, cmp_option_slice_u8))
     (Option<&'a [u16]>, (eq_option_slice_u16, cmp_option_slice_u16))
     (Option<&'a [u32]>, (eq_option_slice_u32, cmp_option_slice_u32))
     (Option<&'a [u64]>, (eq_option_slice_u64, cmp_option_slice_u64))
     (Option<&'a [u128]>, (eq_option_slice_u128, cmp_option_slice_u128))
+    (Option<&'a [usize]>, (eq_option_slice_usize, cmp_option_slice_usize))
     (Option<&'a [i8]>, (eq_option_slice_i8, cmp_option_slice_i8))
     (Option<&'a [i16]>, (eq_option_slice_i16, cmp_option_slice_i16))
     (Option<&'a [i32]>, (eq_option_slice_i32, cmp_option_slice_i32))
     (Option<&'a [i64]>, (eq_option_slice_i64, cmp_option_slice_i64))
     (Option<&'a [i128]>, (eq_option_slice_i128, cmp_option_slice_i128))
+    (Option<&'a [isize]>, (eq_option_slice_isize, cmp_option_slice_isize))
     (Option<&'a [bool]>, (eq_option_slice_bool, cmp_option_slice_bool))
     (Option<&'a [char]>, (eq_option_slice_char, cmp_option_slice_char))
 
