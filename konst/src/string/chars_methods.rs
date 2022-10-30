5f12f11fb5c7d4062bd9ed07d326cd6dcd8ab3f5
rodrimati1992 <rodrimatt1985@gmail.com>
2022-10-30 00:18:49 -0300

    Added `konst::string::char_indices` function
    Added `konst::string::{CharIndices, RCharIndices}` iterators

--- konst/src/string/chars_methods.rs
+++ konst/src/string/chars_methods.rs
@@ -55,16 +55,19 @@ pub(super) const fn string_to_char(s: &str) -> char {
 /// assert_eq!(REV, &['r', 'a', 'b']);
 ///
 /// ```
+#[cfg_attr(feature = "docsrs", doc(cfg(feature = "iter")))]
 pub const fn chars(string: &str) -> Chars<'_> {
     Chars { this: string }
 }
 
 /// Const equivalent of [`core::str::Chars`]
+#[cfg_attr(feature = "docsrs", doc(cfg(feature = "iter")))]
 pub struct Chars<'a> {
     this: &'a str,
 }
 
 /// Const equivalent of `core::iter::Rev<core::str::Chars<'_>>`
+#[cfg_attr(feature = "docsrs", doc(cfg(feature = "iter")))]
 pub struct RChars<'a> {
     this: &'a str,
 }
@@ -136,5 +139,125 @@ impl<'a> Chars<'a> {
 }
 
 impl<'a> RChars<'a> {
+    chars_shared! {is_forward = false}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Cosnt equivalent of [`str::char_indices`].
+///
+/// # Example
+///
+/// ```rust
+/// use konst::string;
+/// use konst::iter::collect_const;
+///
+/// const CHARS: &[(usize, char)] =
+///     &collect_const!((usize, char) => string::char_indices("个bar人"));
+///
+/// const REV: &[(usize, char)] =
+///     &collect_const!((usize, char) => string::char_indices("个bar人").rev());
+///
+/// assert_eq!(CHARS, &[(0, '个'), (3, 'b'), (4, 'a'), (5, 'r'), (6, '人')]);
+/// assert_eq!(REV, &[(6, '人'), (5, 'r'), (4, 'a'), (3, 'b'), (0, '个')]);
+///
+/// ```
+#[cfg_attr(feature = "docsrs", doc(cfg(feature = "iter")))]
+pub const fn char_indices(string: &str) -> CharIndices<'_> {
+    CharIndices {
+        this: string,
+        start_offset: 0,
+    }
+}
+
+/// Const equivalent of [`core::str::CharIndices`]
+#[cfg_attr(feature = "docsrs", doc(cfg(feature = "iter")))]
+pub struct CharIndices<'a> {
+    this: &'a str,
+    start_offset: usize,
+}
+
+/// Const equivalent of `core::iter::Rev<core::str::CharIndices<'_>>`
+#[cfg_attr(feature = "docsrs", doc(cfg(feature = "iter")))]
+pub struct RCharIndices<'a> {
+    this: &'a str,
+    start_offset: usize,
+}
+
+impl ConstIntoIter for CharIndices<'_> {
+    type Kind = IsIteratorKind;
+}
+impl ConstIntoIter for RCharIndices<'_> {
+    type Kind = IsIteratorKind;
+}
+
+macro_rules! chars_shared {
+    (is_forward = $is_forward:ident) => {
+        iterator_shared! {
+            is_forward = $is_forward,
+            item = (usize, char),
+            iter_forward = CharIndices<'a>,
+            iter_reversed = RCharIndices<'a>,
+            next(self){
+                if self.this.is_empty() {
+                    return None
+                }
+
+                let split_at = __find_next_char_boundary(self.this.as_bytes(), 0);
+                let (prev, next) = string::split_at(self.this, split_at);
+                let ret = Self {
+                    this: next,
+                    start_offset: self.start_offset + split_at,
+                };
+
+                Some(((self.start_offset, string_to_char(prev)), ret))
+            },
+            next_back{
+                if self.this.is_empty() {
+                    return None
+                }
+
+                let split_at = __find_prev_char_boundary(self.this.as_bytes(), self.this.len());
+                let (prev, next) = string::split_at(self.this, split_at);
+                let ret = Self {
+                    this: prev,
+                    start_offset: self.start_offset,
+                };
+
+                Some(((self.start_offset + split_at, string_to_char(next)), ret))
+            },
+            fields = {this, start_offset},
+        }
+    };
+}
+
+impl<'a> CharIndices<'a> {
+    chars_shared! {is_forward = true}
+
+    /// Gets a string slice to the yet-to-be-iterated characters.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use konst::{for_range, option, string};
+    ///
+    /// const S: &str = {
+    ///     let mut iter = string::char_indices("this is fine");
+    ///     for_range!{_ in 0..8 =>
+    ///         (_, iter) = option::unwrap!(iter.next());
+    ///     }
+    ///     iter.as_str()
+    /// };
+    ///
+    /// assert_eq!(S, "fine")
+    ///
+    /// ```
+    ///
+    pub const fn as_str(&self) -> &'a str {
+        self.this
+    }
+}
+
+impl<'a> RCharIndices<'a> {
     chars_shared! {is_forward = false}
 }
--- konst/tests/misc_tests/string_tests/string_chars_tests.rs
+++ konst/tests/misc_tests/string_tests/string_chars_tests.rs
@@ -1,6 +1,21 @@
 use konst::string;
 
 // A string with all char lengths
+//
+// (0, 'f')
+// (1, 'o')
+// (2, 'o')
+// (3, 'ñ')
+// (5, '个')
+// (8, '人')
+// (11, 'b')
+// (12, 'a')
+// (13, 'r')
+// (14, '\u{100000}')
+// (18, 'b')
+// (19, '\u{10ffff}')
+// (23, 'a')
+// (24, 'z')
 const CHAR_LENS: &str = "fooñ个人bar\u{100000}b\u{10FFFF}az";
 
 #[test]
@@ -85,6 +100,112 @@ fn chars_rev_test() {
             ['z', 'a', '\u{10FFFF}', 'b', '\u{100000}']
         );
         assert_eq!([b0, b1, b2, b3, b4], ['f', 'o', 'o', 'ñ', '个']);
+        assert_eq!(iter.rev().as_str(), "人bar");
+    }
+}
+
+#[test]
+fn char_indices_const_fn_test() {
+    use konst::string::{CharIndices, RCharIndices};
+    const fn _is_const_callable<'a>(iter: CharIndices<'a>) {
+        let _: CharIndices<'a> = iter.copy();
+        let _: Option<((usize, char), CharIndices<'a>)> = iter.copy().next();
+        let _: Option<((usize, char), CharIndices<'a>)> = iter.copy().next_back();
+        let _: &'a str = iter.as_str();
+
+        let rev: RCharIndices<'a> = iter.rev();
+        let _: Option<((usize, char), RCharIndices<'a>)> = rev.copy().next();
+        let _: Option<((usize, char), RCharIndices<'a>)> = rev.next_back();
+    }
+}
+
+#[test]
+fn char_indices_test() {
+    {
+        let collected = collect_const_iter!(string::char_indices(CHAR_LENS));
+        assert_eq!(collected, CHAR_LENS.char_indices().collect::<Vec<_>>());
+    }
+    {
+        let mut kiter = string::char_indices(CHAR_LENS);
+        let mut iter = CHAR_LENS.char_indices();
+        let mut kelem;
+
+        assert_eq!(kiter.as_str(), iter.as_str());
+
+        while let Some(elem) = iter.next() {
+            (kelem, kiter) = kiter.next().unwrap();
+            assert_eq!(elem, kelem);
+            assert_eq!(kiter.as_str(), iter.as_str());
+        }
+
+        assert!(kiter.next().is_none());
+    }
+    {
+        let iter: string::CharIndices<'_> = string::char_indices(CHAR_LENS);
+        let (f0, iter) = iter.next_back().unwrap();
+        let (f1, iter) = iter.next_back().unwrap();
+        let (f2, iter) = iter.next_back().unwrap();
+        let (f3, iter) = iter.next_back().unwrap();
+        let (f4, iter) = iter.next_back().unwrap();
+        let (b0, iter) = iter.next().unwrap();
+        let (b1, iter) = iter.next().unwrap();
+        let (b2, iter) = iter.next().unwrap();
+        let (b3, iter) = iter.next().unwrap();
+        let (b4, iter) = iter.next().unwrap();
+
+        assert_eq!(
+            [f0, f1, f2, f3, f4],
+            [
+                (24, 'z'),
+                (23, 'a'),
+                (19, '\u{10ffff}'),
+                (18, 'b'),
+                (14, '\u{100000}')
+            ]
+        );
+
+        assert_eq!(
+            [b0, b1, b2, b3, b4],
+            [(0, 'f'), (1, 'o'), (2, 'o'), (3, 'ñ'), (5, '个')]
+        );
+        assert_eq!(iter.as_str(), "人bar");
+    }
+}
+
+#[test]
+fn char_indices_rev_test() {
+    {
+        let rev = collect_const_iter!(string::char_indices(CHAR_LENS).rev());
+        assert_eq!(rev, CHAR_LENS.char_indices().rev().collect::<Vec<_>>());
+    }
+    {
+        let iter: string::RCharIndices<'_> = string::char_indices(CHAR_LENS).rev();
+        let (f0, iter) = iter.next().unwrap();
+        let (f1, iter) = iter.next().unwrap();
+        let (f2, iter) = iter.next().unwrap();
+        let (f3, iter) = iter.next().unwrap();
+        let (f4, iter) = iter.next().unwrap();
+        let (b0, iter) = iter.next_back().unwrap();
+        let (b1, iter) = iter.next_back().unwrap();
+        let (b2, iter) = iter.next_back().unwrap();
+        let (b3, iter) = iter.next_back().unwrap();
+        let (b4, iter) = iter.next_back().unwrap();
+
+        assert_eq!(
+            [f0, f1, f2, f3, f4],
+            [
+                (24, 'z'),
+                (23, 'a'),
+                (19, '\u{10ffff}'),
+                (18, 'b'),
+                (14, '\u{100000}')
+            ]
+        );
+
+        assert_eq!(
+            [b0, b1, b2, b3, b4],
+            [(0, 'f'), (1, 'o'), (2, 'o'), (3, 'ñ'), (5, '个')]
+        );
         assert_eq!(iter.rev().as_str(), "人bar");
     }
 }
