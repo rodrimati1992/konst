8a8b83f71c370391f70bc4cc687d25a8f7e2cff2
rodrimati1992 <rodrimatt1985@gmail.com>
2022-11-11 01:31:35 -0300

    Update lib.rs

--- konst/src/lib.rs
+++ konst/src/lib.rs
@@ -306,8 +306,8 @@
 // clippy's opinionated BS
 #![allow(clippy::needless_doctest_main)]
 #![allow(clippy::init_numbered_fields)]
-#![forbid(clippy::missing_const_for_fn)]
 ////////////
+#![forbid(clippy::missing_const_for_fn)]
 #![cfg_attr(
     feature = "nightly_mut_refs",
     feature(const_mut_refs, const_slice_from_raw_parts_mut)
