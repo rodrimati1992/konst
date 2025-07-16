9c41008425a3866df4628de64d57db672d357c34
rodrimati1992 <rodrimatt1985@gmail.com>
2025-07-16 18:23:29 -0300

    Removed konst::ptr::{as_ref, as_mut}

--- konst/src/ptr.rs
+++ konst/src/ptr.rs
@@ -2,68 +2,11 @@
 //!
 //! # Removed in 0.4.0
 //!
-//! `is_null` was removed because it was deprecated in 0.3.0 for unsoundness.
-
-/// Const equivalent of
-/// [`<*const>::as_ref`](https://doc.rust-lang.org/std/primitive.pointer.html#method.as_ref)
-///
-/// # Safety
-///
-/// This function has the same safety requirements as
-/// [`<*const T>::as_ref`](https://doc.rust-lang.org/1.55.0/std/primitive.pointer.html#safety)
-///
-/// # Example
-///
-/// ```rust
-/// use konst::ptr;
-///
-/// use core::ptr::null;
-///
-/// const NONE: Option<&u8> = unsafe{ ptr::as_ref(null()) };
-/// const SOME: Option<&u8> = unsafe{ ptr::as_ref(&100) };
-///
-/// assert_eq!(NONE, None);
-/// assert_eq!(SOME, Some(&100));
-///
-///
-/// ```
-pub const unsafe fn as_ref<'a, T: ?Sized>(ptr: *const T) -> Option<&'a T> {
-    core::mem::transmute(ptr)
-}
-
-/// Const equivalent of
-/// [`<*const>::as_mut`](https://doc.rust-lang.org/std/primitive.pointer.html#method.as_mut)
-///
-/// # Safety
-///
-/// This function has the same safety requirements as
-/// [`<*mut T>::as_mut`](https://doc.rust-lang.org/1.55.0/std/primitive.pointer.html#safety-13).
-///
-/// # Example
-///
-/// ```rust
-/// use konst::ptr;
-///
-/// assert_eq!(ARR, [83, 91, 104]);
-///
-/// const ARR: [u8; 3] = unsafe {
-///     let mut arr = [13, 21, 34];
-///     mutate(&mut arr[0]);
-///     mutate(&mut arr[1]);
-///     mutate(&mut arr[2]);
-///     mutate(std::ptr::null_mut()); // no-op
-///     arr
-/// };
-///
-/// const unsafe fn mutate(x: *mut u8) {
-///     if let Some(mutt) = ptr::as_mut(x) {
-///         *mutt += 70;
-///     }
-/// }
-/// ```
-pub const unsafe fn as_mut<'a, T: ?Sized>(ptr: *mut T) -> Option<&'a mut T> {
-    core::mem::transmute(ptr)
-}
+//! - `is_null` was removed because it was deprecated in 0.3.0 for unsoundness.
+//! - `as_ref`: [method was const-stabilized in 1.84.0](
+//!              https://doc.rust-lang.org/std/primitive.pointer.html#method.as_ref)
+//! - `as_mut`: [method was const-stabilized in 1.84.0](
+//!              https://doc.rust-lang.org/std/primitive.pointer.html#method.as_mut)
 
 /// Const equivalents of [`NonNull`](core::ptr::NonNull) methods.
 ///
@@ -72,10 +15,9 @@ pub const unsafe fn as_mut<'a, T: ?Sized>(ptr: *mut T) -> Option<&'a mut T> {
 /// These functions were removed in 0.4.0 because there is an equivalent
 /// const fn in the standard library:
 ///
-/// - `as_ref`: [NonNull::as_ref]
-/// - `as_mut`: [NonNull::as_mut]
+/// - `as_ref`: [core::ptr::NonNull::as_ref]
+/// - `as_mut`: [core::ptr::NonNull::as_mut]
 ///
-/// 
 /// `new` was removed because it was deprecated in 0.3.0 for unsoundness.
 ///
 pub mod nonnull {
