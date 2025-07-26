883fff8936e1359f7442a16004f82e58235a0664
rodrimati1992 <rodrimatt1985@gmail.com>
2025-07-26 19:03:28 -0300

    Added `konst::iter::{RepeatN, repeat_n}` iterator adaptor
    Added test for the above

--- konst/src/iter/iterator_adaptors.rs
+++ konst/src/iter/iterator_adaptors.rs
@@ -25,7 +25,9 @@ impl<T> ConstIntoIter for Repeat<T> {
     type Kind = IsIteratorKind;
     type IntoIter = Self;
     type Item = T;
-    const ITEMS_NEED_DROP: bool = core::mem::needs_drop::<T>();
+
+    // since the constructor requires T: Copy, it doesn't need dropping
+    const ITEMS_NEED_DROP: bool = false;
 }
 
 impl<T: Copy> Repeat<T> {
@@ -44,5 +46,64 @@ impl<T: Copy> Repeat<T> {
     /// Clones the iterator
     pub const fn copy(&self) -> Self {
         Self(self.0)
+    }
+}
+
+/////////////////////////////////////////////////////
+
+/// Const analog of [`core::iter::repeat_n`],
+/// except that this requires the repeated value to impl `Copy`
+/// (instead of `Clone`).
+///
+/// # Example
+///
+/// ```rust
+/// use konst::iter::{self, collect_const};
+///
+/// const ARR: &[u8] = &collect_const!(u8 => iter::repeat_n(8, 3));
+///
+/// assert_eq!(ARR, &[8, 8, 8]);
+/// ```
+pub const fn repeat_n<T: Copy>(val: T, count: usize) -> RepeatN<T> {
+    RepeatN { val, count }
+}
+
+/// Const analog of [`core::iter::RepeatN`],
+/// constructed by [`repeat_n`](crate::iter::repeat_n).
+pub struct RepeatN<T> {
+    val: T,
+    count: usize,
+}
+
+impl<T> ConstIntoIter for RepeatN<T> {
+    type Kind = IsIteratorKind;
+    type IntoIter = Self;
+    type Item = T;
+
+    // since the constructor requires T: Copy, it doesn't need dropping
+    const ITEMS_NEED_DROP: bool = false;
+}
+
+impl<T: Copy> RepeatN<T> {
+    /// Gets the next element in the iterator
+    pub const fn next(&mut self) -> Option<T> {
+        if let Some(ncount) = self.count.checked_sub(1) {
+            self.count = ncount;
+            Some(self.val)
+        } else {
+            None
+        }
+    }
+    /// Gets the next element in the iterator
+    pub const fn next_back(&mut self) -> Option<T> {
+        self.next()
+    }
+    /// Reverses the iterator
+    pub const fn rev(self) -> Self {
+        self
+    }
+    /// Clones the iterator
+    pub const fn copy(&self) -> Self {
+        Self { ..*self }
     }
 }
--- konst/tests/misc_tests/iter_mod/iter_adaptor_tests.rs
+++ konst/tests/misc_tests/iter_mod/iter_adaptor_tests.rs
@@ -23,3 +23,44 @@ fn test_repeat() {
 
     assert_eq!(isconst(), [10u8; 8]);
 }
+
+#[test]
+fn test_repeat_n() {
+    const fn isconst() -> [u8; 8] {
+        let mut iter: iter::RepeatN<u8> = iter::repeat_n(10, 12);
+
+        let mut iter_copy: iter::RepeatN<u8> = iter.copy();
+        let a0 = iter_copy.next().unwrap();
+        let a1 = iter.next().unwrap();
+        let b = iter.next_back().unwrap();
+        let iter: iter::RepeatN<u8> = iter.rev();
+        let mut iter: iter::RepeatN<u8> = iter::into_iter!(iter.copy());
+        let c0 = iter.next().unwrap();
+        let c1 = iter.next().unwrap();
+        let d = iter.next_back().unwrap();
+        let mut iter: iter::RepeatN<u8> = iter.rev();
+        let e = iter.next().unwrap();
+        let f = iter.next_back().unwrap();
+
+        [a0, a1, b, c0, c1, d, e, f]
+    }
+
+    assert_eq!(isconst(), [10u8; 8]);
+
+    assert_eq!(
+        iter::collect_const! {char => iter::repeat_n('a', 0)},
+        ['a'; 0]
+    );
+    assert_eq!(
+        iter::collect_const! {char => iter::repeat_n('b', 1)},
+        ['b'; 1]
+    );
+    assert_eq!(
+        iter::collect_const! {char => iter::repeat_n('c', 2)},
+        ['c'; 2]
+    );
+    assert_eq!(
+        iter::collect_const! {char => iter::repeat_n('d', 3)},
+        ['d'; 3]
+    );
+}
