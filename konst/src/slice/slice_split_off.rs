fc267859fd9dd40c971cb1365ab03d8dfe635437
rodrimati1992 <rodrimatt1985@gmail.com>
2025-08-14 04:55:56 -0300

    Added slice::split_off_(first|last)(_mut?) functions
    Added these functions:
    - konst::slice::split_off_first
    - konst::slice::split_off_first_mut
    - konst::slice::split_off_last
    - konst::slice::split_off_last_mut
    
    Added tests for the above methods

--- konst/src/slice/slice_split_off.rs
+++ konst/src/slice/slice_split_off.rs
@@ -67,9 +67,9 @@ where
 ///     
 ///     assert!(slice::split_off_mut(&mut slice, 100..).is_none());
 ///     
-///     assert!(matches!(slice::split_off_mut(&mut slice, ..2).unwrap(), &mut [0, 1]));
-///     assert!(matches!(slice::split_off_mut(&mut slice, ..=3).unwrap(), &mut [2, 3, 4, 5]));
-///     assert!(matches!(slice::split_off_mut(&mut slice, 1..).unwrap(), &mut [7, 8, 9, 10]));
+///     assert!(matches!(slice::split_off_mut(&mut slice, ..2), Some(&mut [0, 1])));
+///     assert!(matches!(slice::split_off_mut(&mut slice, ..=3), Some(&mut [2, 3, 4, 5])));
+///     assert!(matches!(slice::split_off_mut(&mut slice, 1..), Some(&mut [7, 8, 9, 10])));
 ///     assert!(matches!(slice, &mut [6]));
 /// };
 ///
@@ -98,4 +98,124 @@ where
     *this = assign;
 
     Some(ret)
+}
+
+/// Const equivalent of
+/// [`<[T]>::split_off_first`](
+/// https://doc.rust-lang.org/std/primitive.slice.html#method.split_off_first)
+///
+/// # Example
+///
+/// ```rust
+/// use konst::slice;
+///
+/// const _: () = {
+///     let mut slice: &[_] = &[0, 1, 2, 3];
+///     
+///     assert!(slice::split_off_first(&mut [0u8; 0].as_slice()).is_none());
+///     
+///     assert!(matches!(slice::split_off_first(&mut slice), Some(&0)));
+///     assert!(matches!(slice::split_off_first(&mut slice), Some(&1)));
+///     assert!(matches!(slice, &[2, 3]));
+/// };
+///
+/// ```
+///
+pub const fn split_off_first<'a, T>(this: &mut &'a [T]) -> Option<&'a T> {
+    if let [first, rem @ ..] = *this {
+        *this = rem;
+        Some(first)
+    } else {
+        None
+    }
+}
+
+/// Const equivalent of
+/// [`<[T]>::split_off_first_mut`](
+/// https://doc.rust-lang.org/std/primitive.slice.html#method.split_off_first_mut)
+///
+/// # Example
+///
+/// ```rust
+/// use konst::slice;
+///
+/// const _: () = {
+///     let mut slice: &mut [_] = &mut [0, 1, 2, 3];
+///     
+///     assert!(slice::split_off_first_mut(&mut [0u8; 0].as_mut_slice()).is_none());
+///     
+///     assert!(matches!(slice::split_off_first_mut(&mut slice), Some(&mut 0)));
+///     assert!(matches!(slice::split_off_first_mut(&mut slice), Some(&mut 1)));
+///     assert!(matches!(slice, &mut [2, 3]));
+/// };
+///
+/// ```
+///
+pub const fn split_off_first_mut<'a, T>(this: &mut &'a mut [T]) -> Option<&'a mut T> {
+    if let [first, rem @ ..] = mem::replace(this, &mut []) {
+        *this = rem;
+        Some(first)
+    } else {
+        None
+    }
+}
+
+/// Const equivalent of
+/// [`<[T]>::split_off_last`](
+/// https://doc.rust-lang.org/std/primitive.slice.html#method.split_off_last)
+///
+/// # Example
+///
+/// ```rust
+/// use konst::slice;
+///
+/// const _: () = {
+///     let mut slice: &[_] = &[0, 1, 2, 3];
+///     
+///     assert!(slice::split_off_last(&mut [0u8; 0].as_slice()).is_none());
+///     
+///     assert!(matches!(slice::split_off_last(&mut slice), Some(&3)));
+///     assert!(matches!(slice::split_off_last(&mut slice), Some(&2)));
+///     assert!(matches!(slice, &[0, 1]));
+/// };
+///
+/// ```
+///
+pub const fn split_off_last<'a, T>(this: &mut &'a [T]) -> Option<&'a T> {
+    if let [rem @ .., last] = *this {
+        *this = rem;
+        Some(last)
+    } else {
+        None
+    }
+}
+
+/// Const equivalent of
+/// [`<[T]>::split_off_last_mut`](
+/// https://doc.rust-lang.org/std/primitive.slice.html#method.split_off_last_mut)
+///
+/// # Example
+///
+/// ```rust
+/// use konst::slice;
+///
+/// const _: () = {
+///     let mut slice: &mut [_] = &mut [0, 1, 2, 3];
+///     
+///     assert!(slice::split_off_last_mut(&mut [0u8; 0].as_mut_slice()).is_none());
+///     
+///     assert!(matches!(slice::split_off_last_mut(&mut slice), Some(&mut 3)));
+///     assert!(matches!(slice::split_off_last_mut(&mut slice), Some(&mut 2)));
+///     assert!(matches!(slice, &mut [0, 1]));
+/// };
+///
+/// ```
+///
+pub const fn split_off_last_mut<'a, T>(this: &mut &'a mut [T]) -> Option<&'a mut T> {
+    if let [rem @ .., last] = mem::replace(this, &mut []) {
+        *this = rem;
+        Some(last)
+    } else {
+        None
+    }
 }
--- konst/tests/misc_tests/slice_tests/slice_split_off_tests.rs
+++ konst/tests/misc_tests/slice_tests/slice_split_off_tests.rs
@@ -129,3 +129,58 @@ fn split_off_mut_zst_test() {
         }
     }
 }
+
+#[test]
+fn split_off_first_last_test() {
+    const fn _constness() {
+        let mut slice: &[u8] = &[];
+        slice::split_off_first(&mut slice);
+        slice::split_off_last(&mut slice);
+    }
+
+    let source: [usize; 10] = std::array::from_fn(|i| i);
+
+    for len in 0..=source.len() {
+        macro_rules! with_method {
+            ($method:ident) => {{
+                let mut slice_kon = &source[..len];
+                let mut slice_std = &source[..len];
+
+                assert_eq!(slice::$method(&mut slice_kon), slice_std.$method());
+
+                assert_eq!(slice_kon, slice_std);
+            }};
+        }
+
+        with_method! {split_off_first}
+        with_method! {split_off_last}
+    }
+}
+
+#[test]
+fn split_off_first_last_mut_test() {
+    const fn _constness() {
+        let mut slice: &mut [u8] = &mut [];
+        slice::split_off_first_mut(&mut slice);
+        slice::split_off_last_mut(&mut slice);
+    }
+
+    let mut source: [usize; 10] = std::array::from_fn(|i| i);
+
+    for len in 0..=source.len() {
+        macro_rules! with_method {
+            ($method:ident) => {{
+                let mut clone = source;
+                let mut slice_kon = &mut clone[..len];
+                let mut slice_std = &mut source[..len];
+
+                assert_eq!(slice::$method(&mut slice_kon), slice_std.$method());
+
+                assert_eq!(slice_kon, slice_std);
+            }};
+        }
+
+        with_method! {split_off_first_mut}
+        with_method! {split_off_last_mut}
+    }
+}
